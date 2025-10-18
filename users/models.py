9763ae2bde11d60e2a1f5fe3fa42434379de4c4c
sollyaj <soajohworode@gmail.com>
2025-10-18 22:09:13 +0100

    Add user roles and admin-only issue resolution

--- users/models.py
+++ users/models.py
@@ -2,12 +2,13 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class User(AbstractUser):
-    ROLE_CHOICES = (
-        ('citizen', 'Citizen'),
+    ROLE_CHOICES = [
+        ('user', 'User'),
         ('admin', 'Admin'),
-    )
-    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='citizen')
+    ]
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='user')
 
     def __str__(self):
         return self.username
+
 
