7e2cd154905dbbb475c5a7afa3dcaf164d68a0dc
sollyaj <soajohworode@gmail.com>
2025-10-19 17:36:55 +0100

    Finished projeect

--- users/views.py
+++ users/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics
+from rest_framework import generics, permissions
 from .serializers import RegisterSerializer
 from django.contrib.auth import get_user_model
 
@@ -7,4 +7,4 @@ User = get_user_model()
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = RegisterSerializer
-
+    permission_classes = [permissions.AllowAny]
